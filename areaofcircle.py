b1a8b73b2dfadd9adbc07767ad80c11ea5f99236
git-vinit <73235335+git-vinit@users.noreply.github.com>
2021-09-03 18:26:21 +0530

    Add files via upload

---
+++ areaofcircle.py
@@ -0,0 +1,4 @@
+PI = 3.14
+r = float(input("Enter the radius of a circle:"))
+area = PI * r * r
+print("the area of circle for entered radius is:", area)
---
+++ fileextensionprinted.py
@@ -0,0 +1,3 @@
+filename = input("Input the Filename: ")
+f_extns = filename.split(".")
+print (f"The extension of the file is : {f_extns[1]} and filename is : {f_extns[0]}")
